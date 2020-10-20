ced10bebbaac5b4db9dafc4d00ed73ac3b7fc719
Mubeen Akhter <mubeendoraaymon@gmail.com>
2020-10-20 15:49:24 +0500

    first commit

---
+++ database/ipos.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 04, 2019 at 07:22 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ipos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_category`
+--
+
+CREATE TABLE `tbl_category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_category`
+--
+
+INSERT INTO `tbl_category` (`cat_id`, `cat_name`) VALUES
+(1, 'Suger');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_invoice`
+--
+
+CREATE TABLE `tbl_invoice` (
+  `invoice_id` int(11) NOT NULL,
+  `cashier_name` varchar(100) NOT NULL,
+  `order_date` date NOT NULL,
+  `time_order` varchar(50) NOT NULL,
+  `total` float NOT NULL,
+  `paid` float NOT NULL,
+  `due` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_invoice`
+--
+
+INSERT INTO `tbl_invoice` (`invoice_id`, `cashier_name`, `order_date`, `time_order`, `total`, `paid`, `due`) VALUES
+(1, 'Mubeen Akhter', '2019-10-20', '11:30', 100, 200, -100);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_invoice_detail`
+--
+
+CREATE TABLE `tbl_invoice_detail` (
+  `id` int(11) NOT NULL,
+  `invoice_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `product_code` char(6) NOT NULL,
+  `product_name` varchar(100) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` float NOT NULL,
+  `total` float NOT NULL,
+  `order_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_invoice_detail`
+--
+
+INSERT INTO `tbl_invoice_detail` (`id`, `invoice_id`, `product_id`, `product_code`, `product_name`, `qty`, `price`, `total`, `order_date`) VALUES
+(1, 1, 1, 'TT0040', 'Suger', 1, 100, 100, '2019-12-04');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_product`
+--
+
+CREATE TABLE `tbl_product` (
+  `product_id` int(11) NOT NULL,
+  `product_code` char(6) NOT NULL,
+  `product_name` varchar(200) NOT NULL,
+  `product_category` varchar(200) NOT NULL,
+  `purchase_price` float(10,0) NOT NULL,
+  `sell_price` float(10,0) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `min_stock` int(11) NOT NULL,
+  `product_satuan` varchar(200) NOT NULL,
+  `description` varchar(200) NOT NULL,
+  `img` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_product`
+--
+
+INSERT INTO `tbl_product` (`product_id`, `product_code`, `product_name`, `product_category`, `purchase_price`, `sell_price`, `stock`, `min_stock`, `product_satuan`, `description`, `img`) VALUES
+(1, 'TT0040', 'Suger', 'Suger', 90, 100, 7, 5, 'Kg', 'Lorem Ipsum', '5f8ea04d614cf.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_satuan`
+--
+
+CREATE TABLE `tbl_satuan` (
+  `kd_satuan` int(2) NOT NULL,
+  `nm_satuan` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_satuan`
+--
+
+INSERT INTO `tbl_satuan` (`kd_satuan`, `nm_satuan`) VALUES
+(1, 'Kg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_user`
+--
+
+CREATE TABLE `tbl_user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `fullname` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `role` varchar(15) NOT NULL,
+  `is_active` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_user`
+--
+
+INSERT INTO `tbl_user` (`user_id`, `username`, `fullname`, `password`, `role`, `is_active`) VALUES
+(1, 'admin', 'admin@admin.com', 'admin', 'Admin', 1),
+(2, 'user', 'user@user.com', 'user', 'Operator', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  ADD PRIMARY KEY (`cat_id`),
+  ADD UNIQUE KEY `cat_name` (`cat_name`),
+  ADD UNIQUE KEY `cat_name_2` (`cat_name`);
+
+--
+-- Indexes for table `tbl_invoice`
+--
+ALTER TABLE `tbl_invoice`
+  ADD PRIMARY KEY (`invoice_id`);
+
+--
+-- Indexes for table `tbl_invoice_detail`
+--
+ALTER TABLE `tbl_invoice_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  ADD PRIMARY KEY (`product_id`),
+  ADD UNIQUE KEY `product_code` (`product_code`,`product_name`);
+
+--
+-- Indexes for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  ADD PRIMARY KEY (`kd_satuan`),
+  ADD UNIQUE KEY `nm_satuan` (`nm_satuan`);
+
+--
+-- Indexes for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_category`
+--
+ALTER TABLE `tbl_category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_invoice`
+--
+ALTER TABLE `tbl_invoice`
+  MODIFY `invoice_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=87;
+
+--
+-- AUTO_INCREMENT for table `tbl_invoice_detail`
+--
+ALTER TABLE `tbl_invoice_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=137;
+
+--
+-- AUTO_INCREMENT for table `tbl_product`
+--
+ALTER TABLE `tbl_product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `tbl_satuan`
+--
+ALTER TABLE `tbl_satuan`
+  MODIFY `kd_satuan` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `tbl_user`
+--
+ALTER TABLE `tbl_user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
